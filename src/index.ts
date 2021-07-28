3b79b22a9084b95fef205764dfddf9f6219bb02b
Lucas Ciruzzi <hi@lshi.ru>
2021-07-28 16:01:30 -0700

    :sparkles: add Button component.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { Anchor } from "./components/Anchor/Anchor";
 export type { AnchorProperties } from "./components/Anchor/AnchorProperties";
+export { Button } from "./components/Button/Button";
 export { Example } from "./components/Example/Example";
 export { validateAll } from "./utils/validateAll/validateAll";
 export { validateAny } from "./utils/validateAny/validateAny";
