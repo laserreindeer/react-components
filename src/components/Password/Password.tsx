0f5dc10e710c6ebc3c2ae1520ec85ad969096c55
Florencia Gonzalez <rightonhana@gmail.com>
2021-07-29 13:55:42 -0700

    :bug: fix input type and snapshot

--- src/components/Password/Password.tsx
+++ src/components/Password/Password.tsx
@@ -5,5 +5,5 @@ import { Input } from "../Input/Input";
  * Input of type "password".
  */
 export const Password = wrap("input")(properties => (
-	<Input type="`password`" {...properties} />
+	<Input type="password" {...properties} />
 ));
